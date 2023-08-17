fe9da61df8f35d39693312b58e4a9787bc988536
Matt Zeiger <matthew.zeiger47@gmail.com>
2023-08-17 18:57:16 -0400

    Adding workflows

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/GameDevPortfolio/', // Update this to match your GitHub Pages repository name
+  build: {
+    outDir: 'dist', // The directory where the production build will be generated
+    assetsDir: '', // Relative path for asset files (e.g., images, fonts)
+    sourcemap: true, // Enable source maps for debugging
+  },
+});
